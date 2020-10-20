67cdf1ff6be9e4742ec4f9500e489e5a57f94119
coldase <jarzu@pm.me>
2020-10-20 18:04:46 +0300

    fixed i hope

--- app.py
+++ app.py
@@ -7,21 +7,16 @@ covid_data = {}
 
 @app.route("/", methods=["GET","POST"])
 def index():
-	global covid_data
 	if request.method == "POST":
 		try:
 			country = request.form["country"]
-			if country in get_country():
+			if country.lower() in get_country():
 				covid_data.update(get_data(country))
-				print(covid_data)
 				return redirect("/")
 			else:
-				covid_data = {"country":"Country not found!"}
 				return redirect("/")
 		except:
-			print("something went wrong")
 			return redirect("/")
-
 	return render_template("index.html", data=covid_data)
 
 if __name__ == "__main__":
